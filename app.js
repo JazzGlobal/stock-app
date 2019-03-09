69075505fbad86fed9e3c22f9d85756c27a3a133
JazzGlobal <waymer147@gmail.com>
2019-03-09 14:28:52 -0500

    Added basic app config

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 //Import Variables (Express, mongoose, request)
 //Set view engine to EJS
 //Set up mongoose database
+var express = require('express'),
+    request = require('request'),
+    mongoose = require('mongoose'),
+    app = express();
 
+//App Config
+mongoose.connect("mongodb://localhost/stock_app", {useNewUrlParser: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Routes
+app.get("/", function(req, res){
+  res.render("index");
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port, process.env.IP, function(){
+  console.log("listening on: " + port);
+});
 //Set up app.listen
