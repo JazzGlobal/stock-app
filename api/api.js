b7e9ffca036a0f3f58145ca89fc111f52cdd5d7d
JazzGlobal <waymer147@gmail.com>
2019-03-14 17:32:18 -0400

    added new api functions

--- api/api.js
+++ api/api.js
@@ -10,6 +10,26 @@ const api =
           callback(data);
         }
       });
+    },
+
+    getCompanyReferenceData(callback){
+      var data = '';
+      request('https://api.iextrading.com/1.0/ref-data/symbols', function(error, response, body){
+        if(!error && response.statusCode == 200){
+          data = JSON.parse(body);
+          callback(data);
+        }
+      });
+    },
+
+    getCompany(symbol, callback){
+      var data = '';
+      request(`https://api.iextrading.com/1.0/stock/${symbol}/company`, function(error, response, body){
+        if(!error && response.statusCode == 200){
+          data = JSON.parse(body);
+          callback(data);
+        }
+      });
     }
   }
 module.exports = api;
