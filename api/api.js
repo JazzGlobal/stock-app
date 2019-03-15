bfc3bab897e00f51e400786ffafebfe0acc1a48f
JazzGlobal <waymer147@gmail.com>
2019-03-15 15:56:01 -0400

    Rewrote company request

--- api/api.js
+++ api/api.js
@@ -3,30 +3,18 @@ var request = require('request');
 const api =
   {
     getNews(callback){
-      var data = '';
       request('https://api.iextrading.com/1.0/stock/market/news/last/5', function(error, response, body){
         if(!error && response.statusCode == 200){
-          data = JSON.parse(body);
-          callback(data);
-        }
-      });
-    },
-
-    getCompanyReferenceData(callback){
-      var data = '';
-      request('https://api.iextrading.com/1.0/ref-data/symbols', function(error, response, body){
-        if(!error && response.statusCode == 200){
-          data = JSON.parse(body);
+          var data = JSON.parse(body);
           callback(data);
         }
       });
     },
 
     getCompany(symbol, callback){
-      var data = '';
-      request(`https://api.iextrading.com/1.0/stock/${symbol}/company`, function(error, response, body){
+      request(`https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote,news`, function(error, response, body){
         if(!error && response.statusCode == 200){
-          data = JSON.parse(body);
+          var data = JSON.parse(body);
           callback(data);
         }
       });
