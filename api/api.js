4f64089986ed9f6e59cac27b75c79c5253633e08
JazzGlobal <waymer147@gmail.com>
2019-03-15 16:51:31 -0400

    add company to batch request

--- api/api.js
+++ api/api.js
@@ -12,7 +12,7 @@ const api =
     },
 
     getCompany(symbol, callback){
-      request(`https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote,news`, function(error, response, body){
+      request(`https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote,news,company`, function(error, response, body){
         if(!error && response.statusCode == 200){
           var data = JSON.parse(body);
           callback(data);
